4b80fbb0e76a5abed01c51f1b872ee0e70d9be87
Leonardo David <leondavidtb@gmail.com>
2021-08-26 16:51:48 -0300

    adicionado botão de deletar

--- src/App.js
+++ src/App.js
@@ -5,31 +5,40 @@ import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
 
-  constructor() {
+  constructor(){
     super();
-    
+
     this.state = {
-      notas: []
+      notas:[]
     }
   }
 
-  criarNota(titulo, texto) {
+  criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas, novaNota];
-    const novoEstato = {
-      notas: novoArrayNotas,
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
     }
-    this.setState(novoEstato);
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas:arrayNotas})
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas 
+        apagarNota={this.deletarNota.bind(this)}
+        notas={this.state.notas}/>
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,32 +2,34 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.titulo = "";
-    this.texto = "";
+    this.titulo ="";
+    this.texto ="";
   }
 
   _handleMudancaTitulo(evento){
     evento.stopPropagation();
-    this.titulo = evento.target.value
+    this.titulo = evento.target.value;
   }
 
-  _handleMudancaTexto(evento) {
+  _handleMudancaTexto(evento){
     evento.stopPropagation();
-    this.texto = evento.target.value
+    this.texto = evento.target.value;
   }
 
-  _criarNota(evento) {
+  _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
     this.props.criarNota(this.titulo, this.texto);
-
+    
   }
 
   render() {
     return (
-      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
@@ -39,7 +41,6 @@ class FormularioCadastro extends Component {
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
           onChange={this._handleMudancaTexto.bind(this)}
-
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
